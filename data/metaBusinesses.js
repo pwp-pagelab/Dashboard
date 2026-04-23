8cea9142474f99f3698d0a3a063a2327d652cb89
pwp-pagelab <pwp@postwithpassion.com>
2026-04-23 14:12:47 +0300

    Update metaBusinesses.js

--- data/metaBusinesses.js
+++ data/metaBusinesses.js
@@ -1,6 +1,6 @@
 export const metaBusinesses = {
-  PWP_MAIN: process.env.META_BUSINESS_ID_PWP_MAIN,
-  PWP_SECOND: process.env.META_BUSINESS_ID_PWP_SECOND
+  PWP_MAIN: process.env.META_BUSINESS_ID_PWP_MAIN || null,
+  PWP_SECOND: process.env.META_BUSINESS_ID_PWP_SECOND || null
 }
 
 export function getMetaBusinessId(businessKey) {
