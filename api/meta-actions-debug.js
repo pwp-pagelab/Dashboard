c628764d510f41c89252214af2be48ccc2e6c38d
pwp-pagelab <pwp@postwithpassion.com>
2026-04-27 16:43:08 +0300

    Update meta-actions-debug.js

--- api/meta-actions-debug.js
+++ api/meta-actions-debug.js
@@ -1,6 +1,9 @@
 export default function handler(req, res) {
+  const accessToken = process.env.META_ACCESS_TOKEN
+
   res.status(200).json({
     ok: true,
-    message: 'route alive'
+    hasToken: Boolean(accessToken),
+    tokenLength: accessToken ? String(accessToken).length : 0
   })
 }
