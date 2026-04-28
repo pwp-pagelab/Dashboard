1cc07c78b67597d89871a3a03e94c08973915688
pwp-pagelab <pwp@postwithpassion.com>
2026-04-28 13:33:45 +0300

    Update clients.js

--- data/clients.js
+++ data/clients.js
@@ -17,25 +17,26 @@ export const clients = [
     }
   },
   {
-    {
-  id: 'clientslug',
-  name: 'Client Name',
-  metaBusinessKey: 'PWP_MAIN',
-  metaMatch: {
-    type: 'includes',
-    value: 'match_value'
+    id: 'calistrafitness',
+    name: 'Calistra',
+    reportingStartDate: '2026-04-04',
+    tiktokAdvertiserId: '7626676131768369172',
+    metaBusinessKey: 'PWP_SECOND',
+    metaMatch: {
+      type: 'includes',
+      value: 'calistra'
+    },
+    snapMatch: {
+      type: 'includes',
+      value: 'Calistra'
+    },
+    platforms: {
+      meta: { enabled: true },
+      google: { enabled: false },
+      tiktok: { enabled: true },
+      snapchat: { enabled: true }
+    }
   },
-  snapMatch: {
-    type: 'includes',
-    value: 'Client Name'
-  },
-  platforms: {
-    meta: { enabled: true },
-    google: { enabled: true },
-    tiktok: { enabled: false },
-    snapchat: { enabled: true }
-  }
-},
   {
     id: 'pwp',
     name: 'PWP',
@@ -49,6 +50,235 @@ export const clients = [
       meta: { enabled: true },
       google: { enabled: false },
       tiktok: { enabled: false },
+      snapchat: { enabled: false }
+    }
+  },
+
+  {
+    id: 'ygii',
+    name: 'Ygii',
+    reportingStartDate: '2026-01-01',
+    tiktokAdvertiserId: '7132464754412634114',
+    platforms: {
+      meta: { enabled: false },
+      google: { enabled: false },
+      tiktok: { enabled: true },
+      snapchat: { enabled: false }
+    }
+  },
+  {
+    id: 'pwp-11081115',
+    name: 'شركة تواصل بشغف للتسويق11081115',
+    reportingStartDate: '2026-01-01',
+    tiktokAdvertiserId: '7265651862102130690',
+    platforms: {
+      meta: { enabled: false },
+      google: { enabled: false },
+      tiktok: { enabled: true },
+      snapchat: { enabled: false }
+    }
+  },
+  {
+    id: 'pwp-1108',
+    name: 'شركة تواصل بشغف للتسويق1108',
+    reportingStartDate: '2026-01-01',
+    tiktokAdvertiserId: '7299080289240989697',
+    platforms: {
+      meta: { enabled: false },
+      google: { enabled: false },
+      tiktok: { enabled: true },
+      snapchat: { enabled: false }
+    }
+  },
+  {
+    id: 'yamama-red-bricks',
+    name: 'شركة اليمامة للطوب الأحمر0522',
+    reportingStartDate: '2026-01-01',
+    tiktokAdvertiserId: '7371810298619248641',
+    platforms: {
+      meta: { enabled: false },
+      google: { enabled: false },
+      tiktok: { enabled: true },
+      snapchat: { enabled: false }
+    }
+  },
+  {
+    id: 'yfc',
+    name: 'YFC',
+    reportingStartDate: '2026-01-01',
+    tiktokAdvertiserId: '7387700111398207504',
+    platforms: {
+      meta: { enabled: false },
+      google: { enabled: false },
+      tiktok: { enabled: true },
+      snapchat: { enabled: false }
+    }
+  },
+  {
+    id: 'yfc-pwp',
+    name: 'YFC -PWP',
+    reportingStartDate: '2026-01-01',
+    tiktokAdvertiserId: '7389216029110566928',
+    platforms: {
+      meta: { enabled: false },
+      google: { enabled: false },
+      tiktok: { enabled: true },
+      snapchat: { enabled: false }
+    }
+  },
+  {
+    id: 'no7-new-ad-account',
+    name: 'No7 New Ad Account',
+    reportingStartDate: '2026-01-01',
+    tiktokAdvertiserId: '7392149260696961025',
+    platforms: {
+      meta: { enabled: false },
+      google: { enabled: false },
+      tiktok: { enabled: true },
+      snapchat: { enabled: false }
+    }
+  },
+  {
+    id: 'test-yfc',
+    name: 'Test YFC',
+    reportingStartDate: '2026-01-01',
+    tiktokAdvertiserId: '7432982394485178385',
+    platforms: {
+      meta: { enabled: false },
+      google: { enabled: false },
+      tiktok: { enabled: true },
+      snapchat: { enabled: false }
+    }
+  },
+  {
+    id: 'lefane',
+    name: 'Lefane',
+    reportingStartDate: '2026-01-01',
+    tiktokAdvertiserId: '7436708078663401488',
+    platforms: {
+      meta: { enabled: false },
+      google: { enabled: false },
+      tiktok: { enabled: true },
+      snapchat: { enabled: false }
+    }
+  },
+  {
+    id: 'kabab-shakers',
+    name: 'Kabab.Shakers',
+    reportingStartDate: '2026-01-01',
+    tiktokAdvertiserId: '7446000428392955905',
+    platforms: {
+      meta: { enabled: false },
+      google: { enabled: false },
+      tiktok: { enabled: true },
+      snapchat: { enabled: false }
+    }
+  },
+  {
+    id: 'yamm',
+    name: 'Yamm',
+    reportingStartDate: '2026-01-01',
+    tiktokAdvertiserId: '7459712999889240080',
+    platforms: {
+      meta: { enabled: false },
+      google: { enabled: false },
+      tiktok: { enabled: true },
+      snapchat: { enabled: false }
+    }
+  },
+  {
+    id: 'pwp-0416',
+    name: 'شركة تواصل بشغف للتسويق0416',
+    reportingStartDate: '2026-01-01',
+    tiktokAdvertiserId: '7493853887540150289',
+    platforms: {
+      meta: { enabled: false },
+      google: { enabled: false },
+      tiktok: { enabled: true },
+      snapchat: { enabled: false }
+    }
+  },
+  {
+    id: 'teddysa',
+    name: 'TeddySa',
+    reportingStartDate: '2026-01-01',
+    tiktokAdvertiserId: '7506499554309849104',
+    platforms: {
+      meta: { enabled: false },
+      google: { enabled: false },
+      tiktok: { enabled: true },
+      snapchat: { enabled: false }
+    }
+  },
+  {
+    id: 'bk-saudi',
+    name: 'Bk.Saudi',
+    reportingStartDate: '2026-01-01',
+    tiktokAdvertiserId: '7524695081987915777',
+    platforms: {
+      meta: { enabled: false },
+      google: { enabled: false },
+      tiktok: { enabled: true },
+      snapchat: { enabled: false }
+    }
+  },
+  {
+    id: 'il-ksa',
+    name: 'il-ksa',
+    reportingStartDate: '2026-01-01',
+    tiktokAdvertiserId: '7526566002525487120',
+    platforms: {
+      meta: { enabled: false },
+      google: { enabled: false },
+      tiktok: { enabled: true },
+      snapchat: { enabled: false }
+    }
+  },
+  {
+    id: 'first-step-sa',
+    name: 'First Step Sa',
+    reportingStartDate: '2026-01-01',
+    tiktokAdvertiserId: '7550613253618237458',
+    platforms: {
+      meta: { enabled: false },
+      google: { enabled: false },
+      tiktok: { enabled: true },
+      snapchat: { enabled: false }
+    }
+  },
+  {
+    id: 'nc-red-bricks',
+    name: 'NcRedBricks',
+    reportingStartDate: '2026-01-01',
+    tiktokAdvertiserId: '7574421014181085200',
+    platforms: {
+      meta: { enabled: false },
+      google: { enabled: false },
+      tiktok: { enabled: true },
+      snapchat: { enabled: false }
+    }
+  },
+  {
+    id: 'pet-market',
+    name: 'Pet.Market',
+    reportingStartDate: '2026-01-01',
+    tiktokAdvertiserId: '7581033628394799121',
+    platforms: {
+      meta: { enabled: false },
+      google: { enabled: false },
+      tiktok: { enabled: true },
+      snapchat: { enabled: false }
+    }
+  },
+  {
+    id: 'sbsf',
+    name: 'SBSF',
+    reportingStartDate: '2026-01-01',
+    tiktokAdvertiserId: '7582169403874197512',
+    platforms: {
+      meta: { enabled: false },
+      google: { enabled: false },
+      tiktok: { enabled: true },
       snapchat: { enabled: false }
     }
   }
